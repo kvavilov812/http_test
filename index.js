8cee8176e0298f1669c7d5f78321d529e120c613
konstantin <konstantin@KAV-HONOR.localdomain>
2022-06-22 01:20:39 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const port = 8000;
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.redirect('/page404');
+});
+
+app.get('/page404', (req, res) => {
+    res.send('404 - not found');
+});
+
+app.get('/hello', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    //res.setDefaultEncoding('utf-8');
+    res.send('Hello');
+});
